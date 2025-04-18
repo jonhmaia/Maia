30186c558ffade22d639eb956381dd6d5254f1df
Joao Marcos Maia <joaomarcosaraujomaia14@gmail.com>
2025-04-18 12:35:42 -0300

    SETTINGS AJUSTADO

--- maia/settings.py
+++ maia/settings.py
@@ -26,7 +26,7 @@ import os
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = ['maia-ztne.onrender.com', 'localhost', '127.0.0.1']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # SECURITY WARNING: don't run with debug turned on in production!
